077082f5a097b4762d925aa7e3503ba0cc36aa0c
fernandesdalton97-glitch <fernandesdalton97@gmail.com>
2026-08-07 15:21:03 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import threading
-from bot.bot import run_bot
-from bot.keep_alive import run_flask
-
-# Roda os 2 juntos
-if __name__ == "__main__":
-    threading.Thread(target=run_flask).start()
-    run_bot()
